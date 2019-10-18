d6122ea0d8fb9970a2e7a2eff89ffb02dc9a5fd7
Alexsander <alexsander11115@gmail.com>
2019-10-18 13:17:28 +0300

    Fix error in routing

--- botsrapreact/src/components/app/App.js
+++ botsrapreact/src/components/app/App.js
@@ -8,7 +8,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route,withRouter} from 'react-router-dom';
 function App() {
 
   return (
@@ -20,7 +20,7 @@ function App() {
             <Route path="/"
                    render={() => <h2>Welcome to StarDB</h2>}
                    exact />
-            <Route path="/contact" component={ContactPage} />
+            <Route path="/contact" component={withRouter(ContactPage)} />
             <Route render={() => <h2>Page not found</h2>} />
           </Switch>
           <Footer/>
--- botsrapreact/src/components/header/header.js
+++ botsrapreact/src/components/header/header.js
@@ -1,9 +1,10 @@
 import React, {Component} from "react";
-import {MDBContainer,MDBIcon,
-    MDBNavbar,MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse,
+import {
+    MDBContainer, MDBIcon,
+    MDBNavbar, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse,
     MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
 } from "mdbreact";
-import { BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './header.css';
 
 class Header extends Component {
@@ -19,57 +20,53 @@ class Header extends Component {
 
     render() {
         return (
-            <Router>
+            <MDBNavbar color="default-color" dark expand="md">
+                <MDBContainer>
+                    <MDBNavbarToggler onClick={this.toggleCollapse}/>
+                    <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+                        <MDBNavbarNav left>
+                            <MDBNavItem active>
+                                <MDBNavLink to="/">Home</MDBNavLink>
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBNavLink to="/personal-cabinet">Личный кабинет</MDBNavLink>
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBNavLink to="/customer-info">Личные данные</MDBNavLink>
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBNavLink to="/balance">Баланс</MDBNavLink>
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBNavLink to="/orders">Заказы</MDBNavLink>
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBNavLink to="/order-history">Моя переписка</MDBNavLink>
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBNavLink to="/document">Документы</MDBNavLink>
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBNavLink to="/contact">Контакты</MDBNavLink>
+                            </MDBNavItem>
+                        </MDBNavbarNav>
+                        <MDBNavbarNav right>
+                            <MDBNavItem>
+                                <MDBDropdown>
+                                    <MDBDropdownToggle nav caret>
+                                        <MDBIcon icon="user"/>
+                                    </MDBDropdownToggle>
+                                    <MDBDropdownMenu className="dropdown-default">
+                                        <MDBDropdownItem href="/login">Войти</MDBDropdownItem>
+                                        <MDBDropdownItem href="/logout">Выйти</MDBDropdownItem>
+                                    </MDBDropdownMenu>
+                                </MDBDropdown>
+                            </MDBNavItem>
+                        </MDBNavbarNav>
+                    </MDBCollapse>
+                </MDBContainer>
+            </MDBNavbar>
 
-
-                    <MDBNavbar color="default-color" dark expand="md">
-                        <MDBContainer>
-                            <MDBNavbarToggler onClick={this.toggleCollapse}/>
-                            <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-                                <MDBNavbarNav left>
-                                    <MDBNavItem active>
-                                        <MDBNavLink to="/">Home</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="/personal-cabinet">Личный кабинет</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="/customer-info">Личные данные</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="/balance">Баланс</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="/orders">Заказы</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="/order-history">Моя переписка</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="/document">Документы</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="/contact">Контакты</MDBNavLink>
-                                    </MDBNavItem>
-                                </MDBNavbarNav>
-                                <MDBNavbarNav right>
-                                    <MDBNavItem>
-                                        <MDBDropdown>
-                                            <MDBDropdownToggle nav caret>
-                                                <MDBIcon icon="user"/>
-                                            </MDBDropdownToggle>
-                                            <MDBDropdownMenu className="dropdown-default">
-                                                <MDBDropdownItem href="/login">Войти</MDBDropdownItem>
-                                                <MDBDropdownItem href="/logout">Выйти</MDBDropdownItem>
-                                            </MDBDropdownMenu>
-                                        </MDBDropdown>
-                                    </MDBNavItem>
-                                </MDBNavbarNav>
-                            </MDBCollapse>
-                        </MDBContainer>
-                    </MDBNavbar>
-
-            </Router>
         );
     }
 }
