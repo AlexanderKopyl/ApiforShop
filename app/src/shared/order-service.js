c19c00060669cd85169e7a72f144a413caa7f1cf
fomenkoyegor <fomenkoyegor@gmail.com>
2019-11-06 12:48:38 +0200

    rev to class

--- app/src/shared/order-service.js
+++ app/src/shared/order-service.js
@@ -21,7 +21,7 @@ export const orders_products = async (order_id) => {
     return await data.json();
 };
 
-export const orderInfo = async (match) =>{
+export const orderInfo = async (match) => {
 
     const data = await fetch(`${config.url}orders/${match.params.id}`, {
         headers: {
@@ -31,3 +31,55 @@ export const orderInfo = async (match) =>{
 
     return await data.json();
 };
+
+
+class OrderService {
+    constructor(url) {
+        this.url = url
+    }
+
+    async getAll() {
+        try {
+            const res = await fetch(`${this.url}orders/order_products/${order_id}`, {
+                headers: {'Authorization': 'Bearer ' + fun.getItem('auth_token')}
+            });
+            return res.json()
+        } catch (e) {
+            throw  new Error(e)
+        }
+    }
+
+    async getId(id) {
+        try {
+            const res = await fetch(`${this.url}orders/order_products/${id}`, {
+                headers: {
+                    'Authorization': 'Bearer ' + fun.getItem('auth_token')
+                }
+            });
+            return res.json();
+        } catch (e) {
+            throw  new Error(e)
+        }
+    }
+
+    async getInfo(match) {
+        try {
+            const res = await fetch(`${this.url}orders/${match.params.id}`, {
+                headers: {
+                    'Authorization': 'Bearer ' + fun.getItem('auth_token')
+                }
+            });
+
+            return res.json();
+        } catch (e) {
+            throw  new Error(e)
+        }
+    }
+}
+
+import {url} from '../app.config';
+
+export const orderService = new OrderService(url);
+// orderService.getAll().then(res=>console.log(res));
+// orderService.getId(12).then(res=>console.log(res));
+// orderService.getInfo('match').then(res=>console.log(res));
