5d924d73f09d549a681ec5ee6a607b3482c15c1f
Alexsander <alexsander11115@gmail.com>
2019-11-25 13:37:02 +0200

    Relocate App

--- app/src/app.config.js
+++
@@ -1,7 +0,0 @@
-import fun from "./lib/function";
-
-export default {
-    // url: 'https://api.zoocomplex.com.ua/api/',
-    url: 'http://localhost:3001/api/',
-    user_id: fun.getItem('user_id'),
-}
