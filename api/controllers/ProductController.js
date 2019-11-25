4ed13ed2e0956bbf084c97ba864b245c8781a644
Alexsander <alexsander11115@gmail.com>
2019-11-25 16:54:59 +0200

    Change product controller

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -19,8 +19,8 @@ const fun = require("../lib/function");
 
 const log4js = require("log4js");
 log4js.configure({
-    appenders: { cheese: { type: "file", filename: "error.log"}},
-    categories: { default: { appenders: ["cheese",], level: "error"}},
+    appenders: {cheese: {type: "file", filename: "error.log"}},
+    categories: {default: {appenders: ["cheese",], level: "error"}},
 },);
 
 const log = log4js.getLogger("product");
@@ -28,46 +28,59 @@ const log = log4js.getLogger("product");
 let result = null;
 
 ProductDescription.hasMany(Product, {foreignKey: "product_id"});
-Product.belongsTo(ProductDescription, {foreignKey: "product_id",targetKey: "product_id"});
+Product.belongsTo(ProductDescription, {foreignKey: "product_id", targetKey: "product_id"});
 
 exports.products = async (req, res, next) => {
-    try{
-        result = await Product.findAll({where: {status: {[Op.gt]: 0}},include: [ProductDescription]});
-    }catch (e) {
+    try {
+        result = await Product.findAll({
+            where: {status: {[Op.gt]: 0}},
+            attributes: ["product_id", "model", "image", "manufacturer_id", "price","quantity","viewed"],
+            include: [
+                {
+                    model: ProductDescription,
+                    where: {language_id: 1},
+                }
+            ]
+        });
+    } catch (e) {
         log.error("Error: " + e.message,);
-    }
-    finally {
-        if(result !== null){
+    } finally {
+        if (result !== null) {
             res.json({
                 message: "Products find",
                 result_code: 0,
                 result,
             },);
-        }else{
+        } else {
             res.sendStatus(404);
         }
     }
 };
 
 exports.product = async (req, res, next) => {
-    try{
+    try {
         result = await Product.findOne(
             {
                 where: {product_id: req.params.id},
-                attributes: ["product_id", "model", "image","manufacturer_id","price","weight"],
-                include: [ProductDescription]}
-            );
-    }catch (e) {
+                attributes: ["product_id", "model", "image", "manufacturer_id", "price", "weight","quantity","viewed"],
+                include: [
+                    {
+                        model: ProductDescription,
+                        where: {language_id: 1},
+                    }
+                ]
+            }
+        );
+    } catch (e) {
         log.error("Error: " + e.message,);
-    }
-    finally {
-        if(result !== null){
+    } finally {
+        if (result !== null) {
             res.json({
                 message: "Product find",
                 result_code: 0,
                 result,
             },);
-        }else{
+        } else {
             res.sendStatus(404);
         }
     }
