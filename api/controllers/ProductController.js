02f63bce22cf86f3ff5ff8deb8abd9ffe4edf3fd
Alexsander <alexsander11115@gmail.com>
2019-11-26 15:41:52 +0200

    Change productController

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -106,7 +106,7 @@ exports.product_attributes = async (req, res, next) => {
             include: [
                 {
                     model: AttributeDescription,
-                    attributes: ["name"],
+                    attributes: ["name","attribute_id"],
                     where: {language_id: 1},
                 }
             ]
