d37a2bc1387e529cc4d83ac1e37dd9d5428023b1
Alexsander <alexsander11115@gmail.com>
2019-12-03 11:16:25 +0200

    delete api

--- api/routes/mail/mail.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-const fun = require('../../lib/function');
-let {sendMessage} = require('../../controllers/customerController');
-
-router.post('/',fun.verifyToken,sendMessage);
-
-module.exports = router;
